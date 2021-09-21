d2ee4bb2b8ec8b0ac04b0933fab62bfe308a1f24
DanteAlonsoHT <dante1818@hotmail.com>
2021-09-21 02:56:21 -0500

    Remove unnecessary files created by create-react-app command

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-//component file
 import TodoContainer from "./components/TodoContainer"
+
 ReactDOM.render(<TodoContainer />, document.getElementById("root"))
 
 reportWebVitals();
