689bd108d9c1f9456c8b8aeec5b8f332aac3e111
DanteAlonsoHT <dante1818@hotmail.com>
2021-09-21 20:06:53 -0500

    Add Navbar as component

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+    },
+    {
+      id: 2,
+      path: '/about',
+      text: 'About',
+    },
+  ];
+  return (
+    <nav className="navBar">
+      <ul className="navUl">
+        {links.map((link) => (
+          <li key={link.id}>
+            <NavLink to={link.path} activeClassName="active-link" exact>{link.text}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
